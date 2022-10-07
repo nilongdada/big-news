54fafd49ab20875a6bb474dc3e71cc9c51764212
nilongdada <lxl1021571543@163.com>
2022-10-07 09:43:27 +0800

    完成了头部导航

--- assets/js/user_avatar.js
+++ assets/js/user_avatar.js
@@ -24,7 +24,7 @@ $(function () {
 
   $('#file').on('change', function (e) {
     const fileList = e.target.files // 伪数组
-    if (fileList.length === 0) return layer, msg('请选择图片')
+    if (fileList.length === 0) return layer.msg('请选择图片')
 
     // 需要转成 blob 格式的图片对象
     const blobUrl = URL.createObjectURL(fileList[0])
@@ -33,5 +33,39 @@ $(function () {
       .cropper('destroy') // 销毁旧的裁剪区域
       .attr('src', blobUrl) // 重新设置图片路径
       .cropper(options) // 重新初始化裁剪区域
+  })
+
+
+
+
+
+
+
+  //为确定按钮绑定点击事件
+  $('#btnUpload').on('click', function () {
+    // 1.要拿到用户裁剪之后的头像
+    // 2.要调用接口，上传头像
+    var dataURL = $image
+      .cropper('getCroppedCanvas', { // 创建一个 Canvas 画布
+        width: 100,
+        height: 100
+      })
+      .toDataURL('image/png')       // 将 Canvas 画布上的内容，转化为 base64 格式的字符串
+
+      $.ajax({
+        method: 'PATCH',
+        url: '/my/update/avatar',
+        data: {
+          avatar: dataURL
+        },
+        success: function(res) {
+          // console.log(res)
+          if(res.code !== 0 ) {
+            return layer.msg('更换头像失败！')
+          }
+          window.parent.getUserInfo()
+          layer.msg('更换头像成功！')
+        }
+      })
   })
 })
